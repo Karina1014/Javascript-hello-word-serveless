5fb79eff8a5a810446a1fdaa9866a32b271ae57e
Karina1014 <janneth_wi15@hotmail.com>
2024-11-17 11:23:11 -0500

    feat: :memo: update index.js
    Updated message to indicate it is generated using JavaScript without Dockerfile.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! This message is generated using JavaScript');
+  res.send('Hello, World! This message is generated using JavaScript without Dockerfile');
 });
 
 const port = 3000;
